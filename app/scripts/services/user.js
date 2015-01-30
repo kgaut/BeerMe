c2f86ba10c0ed13fde6b060a92f1a2583be590a7
Kevin Gautreau <kevin@kgaut.net>
2015-01-30 15:18:04 +0100

    ajout service user

---
+++ app/scripts/services/user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name beermeApp.user
+ * @description
+ * # user
+ * Service in the beermeApp.
+ */
+angular.module('beermeApp')
+  .service('user', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
